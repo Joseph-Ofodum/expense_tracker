08add189876581e14451d2727a3c05ae6caa1752
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-02-21 04:02:14 +0100

    working with ListView instead of column, then having a column inside list view

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -13,12 +16,32 @@ class Expenses extends StatefulWidget{
 }
 
 class _ExpenseState extends State<Expenses>{
+  final List<Expense> _registeredExpenses =[
+    Expense(
+      title: 'Lagos',
+      amount: 200000.00,
+      date: DateTime.now(),
+      category: Category.travel
+    ),
+     Expense(
+      title: 'Gas',
+      amount: 500000.00,
+      date: DateTime.now(),
+      category: Category.work
+    ),
+     Expense(
+      title: 'fish',
+      amount: 50000.00,
+      date: DateTime.now(),
+      category: Category.leisure
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-  return const Scaffold(body: Column(children: [
-    Text('data'),
-    Text('data')
+  return  Scaffold(body: Column(children: [
+    const Text('data'),
+  Expanded(child: ExpensesList(expenses: _registeredExpenses,))
   ],),);
   }
   
