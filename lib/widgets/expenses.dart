ead84c120d2e96328302037321d8cfa257abd6cd
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-02-22 00:21:58 +0100

    one way of pulling up a form and saving it

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -30,14 +31,21 @@ class _ExpenseState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAndExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text ('Joseph Expense Tracker'),
+        title: const Text('Joseph Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {},
-          icon: const Icon(Icons.add)),
+          IconButton(
+              onPressed: _openAndExpenseOverlay, icon: const Icon(Icons.add)),
         ],
       ),
       body: Column(
