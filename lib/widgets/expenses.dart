dea88e44087237889e88f2b9997b49e1353b0f13
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-02-21 09:58:07 +0100

    Adding app bar with the + icon

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,48 +1,54 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
-
 
   @override
   State<Expenses> createState() {
     return _ExpenseState();
-  
   }
-  
 }
 
-class _ExpenseState extends State<Expenses>{
-  final List<Expense> _registeredExpenses =[
+class _ExpenseState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
     Expense(
-      title: 'Lagos',
-      amount: 200000.00,
-      date: DateTime.now(),
-      category: Category.travel
-    ),
-     Expense(
-      title: 'Gas',
-      amount: 500000.00,
-      date: DateTime.now(),
-      category: Category.work
-    ),
-     Expense(
-      title: 'fish',
-      amount: 50000.00,
-      date: DateTime.now(),
-      category: Category.leisure
-    ),
+        title: 'Lagos',
+        amount: 200000.00,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: 'Gas',
+        amount: 500000.00,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'fish',
+        amount: 50000.00,
+        date: DateTime.now(),
+        category: Category.leisure),
   ];
 
   @override
   Widget build(BuildContext context) {
-  return  Scaffold(body: Column(children: [
-    const Text('data'),
-  Expanded(child: ExpensesList(expenses: _registeredExpenses,))
-  ],),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text ('Joseph Expense Tracker'),
+        actions: [
+          IconButton(onPressed: () {},
+          icon: const Icon(Icons.add)),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('data'),
+          Expanded(
+              child: ExpensesList(
+            expenses: _registeredExpenses,
+          ))
+        ],
+      ),
+    );
   }
-  
 }
