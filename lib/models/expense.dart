8503a40883eceaf262b4cc219a25e902a554ed10
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-02-21 03:30:24 +0100

    adding enums to model fields

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,17 +1,20 @@
-import 'dart:ffi';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+
+enum Category{leisure, food, work, travel}
 
 class Expense {
   Expense({
     required this.title,
     required this.amount,
-    required this.date
+    required this.date,
+    required this.category
     }): expenseId = uuid.v4();
 
   final String expenseId;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
